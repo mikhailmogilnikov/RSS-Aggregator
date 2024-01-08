d1faafa96d55b0a179db390556632c955504ef32
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-01-08 21:25:57 +0300

    feat: add bootstrap

--- src/init.js
+++ src/init.js
@@ -1,9 +1,6 @@
 // @ts-check
 
-import Example from './Example.js';
-
 export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
+  const element = document.getElementById('body');
+  console.log(element);
 };
